6ba21bbb22c88d6c2c87e4b82d1bbbd8556bb50d
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-05-18 19:54:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("13675555"))
-    API_HASH = os.environ.get("c0da9c346d2c45dbc7ec49a05da9b2b6")
-    BOT_TOKEN = os.environ.get("5555986769:AAG-nNw82PHwBPlPZ5h55d3hnfHxzqc5JeI")
-    LOGS_CHANNEL = int(os.environ.get("-1001615619184"))
-    BOT_OWNER = int(os.environ.get("5591954930"))
-    MONGODB_URL = os.environ.get("mongodb+srv://f2l:f2l@cluster0.fjjge1y.mongodb.net/?retryWrites=true&w=majority")
-    GOFILE_TOKEN = os.environ.get("zPq1O5MunNKBxs33nTfpvmAnQRsB6PJF")
+    APP_ID = "13675555"
+    API_HASH = "c0da9c346d2c45dbc7ec49a05da9b2b6"
+    BOT_TOKEN = "5555986769:AAG-nNw82PHwBPlPZ5h55d3hnfHxzqc5JeI"
+    LOGS_CHANNEL = "-1001615619184"
+    BOT_OWNER = "5591954930"
+    MONGODB_URL = "mongodb+srv://f2l:f2l@cluster0.fjjge1y.mongodb.net/?retryWrites=true&w=majority"
+    GOFILE_TOKEN = "zPq1O5MunNKBxs33nTfpvmAnQRsB6PJF"
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
